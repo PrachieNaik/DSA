73270bd798acd3909901bf4d5fd09275a6ea91e0
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-01-29 00:15:59 +0530

    Create StockBuySell.cpp

---
+++ StockBuySell/StockBuySell.cpp
@@ -0,0 +1,79 @@
+/*
+
+The cost of stock on each day is given in an array A[] of size N. Find all the days on which you buy and sell the stock so that in between those days your profit is maximum.
+
+Assumption: You can buy and sell as many times as you want.
+
+Method: 
+1. Find the local minima and store it as starting index. If not exists, return.
+2. Find the local maxima. and store it as ending index. If we reach the end, set the end as ending index.
+3. Update the solution (Increment count of buy sell pairs)
+4. Repeat the above steps if end is not reached.
+
+
+Time Complexity: O(n), Space Complexity: O(1)
+
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// This function finds the buy sell
+// schedule for maximum profit
+void stockBuySell(int price[], int n)
+{
+	// Prices must be given for at least two days
+	if (n == 1)
+		return;
+
+	// Traverse through given price array
+	int i = 0;
+	while (i < n - 1) {
+
+		// Find Local Minima
+		// Note that the limit is (n-2) as we are
+		// comparing present element to the next element
+		while ((i < n - 1) && (price[i + 1] <= price[i]))
+			i++;
+
+		// If we reached the end, break
+		// as no further solution possible
+		if (i == n - 1)
+			break;
+
+		// Store the index of minima
+		int buy = i++;
+
+		// Find Local Maxima
+		// Note that the limit is (n-1) as we are
+		// comparing to previous element
+		while ((i < n) && (price[i] >= price[i - 1]))
+			i++;
+			
+	    // If we reached the end, sell on last day
+		if (i == n - 1)
+			i--;
+
+		// Store the index of maxima
+		int sell = i;
+
+		cout << "Buy on day: " << buy
+			<< "\t Sell on day: " << sell << endl;
+	}
+}
+
+// Driver code
+int main()
+{
+	// Stock prices on consecutive days
+	int price[] = { 100, 180, 260, 310, 40, 535, 695 };
+	int n = sizeof(price) / sizeof(price[0]);
+
+	// Fucntion call
+	stockBuySell(price, n);
+
+	return 0;
+}
+
