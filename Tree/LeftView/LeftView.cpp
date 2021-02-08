c6436f48d91eb4e7f8819c5258552155ed95caf8
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-02-08 23:57:57 +0530

    Create LeftView.cpp

---
+++ Tree/LeftView/LeftView.cpp
@@ -0,0 +1,97 @@
+/*
+
+Problem statement: Given a singly linked list of N nodes. The task is to find the middle of the linked list.
+
+Approaches:
+
+Method 1 (Use function to print a given level)
+
+    Algorithm: 
+    There are basically two functions in this method. One is to print all nodes at a given level (printGivenLevel), and other is to print level order traversal of the tree (printLevelorder). printLevelorder makes use of printGivenLevel to print nodes at all levels one by one starting from root.
+
+	Time Complexity: O(n^2) in worst case. For a skewed tree, printGivenLevel() takes O(n) time where n is the number of nodes in the skewed tree. So time complexity of printLevelOrder() is O(n) + O(n-1) + O(n-2) + .. + O(1) which is O(n^2). 
+    Space Complexity: O(n) in worst case. For a skewed tree, printGivenLevel() uses O(n) space for call stack. For a Balanced tree, call stack uses O(log n) space, (i.e., height of the balanced tree). 
+
+Method 2 (Using queue)
+
+    Algorithm: 
+    For each node, first the node is visited and then it’s child nodes are put in a FIFO queue. 
+    
+    printLevelorder(tree)
+    1) Create an empty queue q
+    2) temp_node = root (start from root)
+    3) Loop while temp_node is not NULL
+        a) print temp_node->data.
+        b) Enqueue temp_node’s children 
+          (first left then right children) to q
+        c) Dequeue a node from q.
+    
+    Time Complexity: O(n) where n is number of nodes in the binary tree 
+    Space Complexity: O(n) where n is number of nodes in the binary tree 
+
+*/
+
+/* C++ program to print level 
+	order traversal using STL */
+#include <bits/stdc++.h>
+using namespace std;
+
+// A Binary Tree Node
+struct Node
+{
+	int data;
+	struct Node *left, *right;
+};
+
+// Iterative method to find height of Binary Tree
+void printLevelOrder(Node *root)
+{
+	// Base Case
+	if (root == NULL) return;
+
+	// Create an empty queue for level order traversal
+	queue<Node *> q;
+
+	// Enqueue Root and initialize height
+	q.push(root);
+
+	while (q.empty() == false)
+	{
+		// Print front of queue and remove it from queue
+		Node *node = q.front();
+		cout << node->data << " ";
+		q.pop();
+
+		/* Enqueue left child */
+		if (node->left != NULL)
+			q.push(node->left);
+
+		/*Enqueue right child */
+		if (node->right != NULL)
+			q.push(node->right);
+	}
+}
+
+// Utility function to create a new tree node
+Node* newNode(int data)
+{
+	Node *temp = new Node;
+	temp->data = data;
+	temp->left = temp->right = NULL;
+	return temp;
+}
+
+// Driver program to test above functions
+int main()
+{
+	// Let us create binary tree shown in above diagram
+	Node *root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+
+	cout << "Level Order traversal of binary tree is \n";
+	printLevelOrder(root);
+	return 0;
+}
