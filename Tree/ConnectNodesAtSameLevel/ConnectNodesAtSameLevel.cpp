683f4e12d7a7e644fa4839f5843eb06976f63a9d
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-02-19 21:43:58 +0530

    Update ConnectNodesAtSameLevel.cpp

--- Tree/ConnectNodesAtSameLevel/ConnectNodesAtSameLevel.cpp
+++ Tree/ConnectNodesAtSameLevel/ConnectNodesAtSameLevel.cpp
@@ -1,6 +1,7 @@
 /*
 
-Problem statement: Given a binary tree, connect the nodes that are at same level. You'll be given an addition nextRight pointer for the same.Initially, all the nextRight pointers point to garbage values. Your function should set these pointers to point next right for each node.
+Problem statement: Given a binary tree, connect the nodes that are at same level. You'll be given an addition nextRight pointer for the same.
+Initially, all the nextRight pointers point to garbage values. Your function should set these pointers to point next right for each node.
 
 Constraints:
 1 <= Number of nodes <= 100
@@ -42,24 +43,20 @@ Approaches:
        }
     }
 
-    Method 2: Instead of traversing in Pre Order fashion (root, left, right), if we traverse the nextRight node before the left and right children (root, nextRight, left), then we can make sure that all nodes at level i have the nextRight set, before the level i+1 nodes. 
+    Method 2: Instead of traversing in Pre Order fashion (root, left, right), if we traverse the nextRight node before the left and right children 
+    (root, nextRight, left), then we can make sure that all nodes at level i have the nextRight set, before the level i+1 nodes. 
+    
     Auxiliary Space = O(1)
     
 */
 
-/* This function returns the leftmost 
-child of nodes at the same level as p.  
-This function is used to getNExt right 
-of p's right child  If right child of 
-is NULL then this can also be used for 
-the left child */
+/* This function returns the leftmost child of nodes at the same level as p. This function is used to getNExt right of p's right child If right child of 
+is NULL then this can also be used for the left child */
 node *getNextRight(node *p)  
 {  
     node *temp = p->nextRight;  
   
-    /* Traverse nodes at p's level 
-    and find and return the first  
-    node's first child */
+    /* Traverse nodes at p's level and find and return the first node's first child */
     while (temp != NULL)  
     {  
         if (temp->left != NULL)  
@@ -69,13 +66,11 @@ node *getNextRight(node *p)
         temp = temp->nextRight;  
     }  
   
-    // If all the nodes at p's level  
-    // are leaf nodes then return NULL  
+    // If all the nodes at p's level are leaf nodes then return NULL  
     return NULL;  
 }  
   
-/* Sets nextRight of all nodes 
-of a tree with root as p */
+/* Sets nextRight of all nodes of a tree with root as p */
 void connectRecur(node* p)  
 {  
     node *temp;  
@@ -91,19 +86,13 @@ void connectRecur(node* p)
     {  
         node *q = p;  
   
-        /* Connect all childrem nodes of p and 
-        children nodes of all other nodes at 
-        same level as p */
+        /* Connect all childrem nodes of p and children nodes of all other nodes at same level as p */
         while (q != NULL)  
         {  
-            // Set the nextRight pointer 
-            // for p's left child  
+            // Set the nextRight pointer for p's left child  
             if (q->left)  
             {  
-                // If q has right child, then  
-                // right child is nextRight of  
-                // p and we also need to set 
-                // nextRight of right child  
+                // If q has right child, then right child is nextRight of p and we also need to set nextRight of right child  
                 if (q->right)  
                     q->left->nextRight = q->right;  
                 else
@@ -113,13 +102,11 @@ void connectRecur(node* p)
             if (q->right)  
                 q->right->nextRight = getNextRight(q);  
   
-            // Set nextRight for other  
-            // nodes in pre order fashion  
+            // Set nextRight for other nodes in pre order fashion  
             q = q->nextRight;  
         }  
   
-        // start from the first  
-        // node of next level  
+        // start from the first node of next level  
         if (p->left)  
             p = p->left;  
         else if (p->right)  
