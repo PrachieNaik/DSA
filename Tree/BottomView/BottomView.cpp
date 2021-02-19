e96dd24d5788f7e9e77f98d2da0f4d4bb2997592
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-02-19 21:24:49 +0530

    Update BottomView.cpp

--- Tree/BottomView/BottomView.cpp
+++ Tree/BottomView/BottomView.cpp
@@ -2,15 +2,15 @@
 
 Problem statement: Given a Binary Tree, we need to print the bottom view from left to right. A node x is there in output if x is the bottommost node at its horizontal distance. Horizontal distance of left child of a node x is equal to horizontal distance of x minus 1, and that of right child is horizontal distance of x plus 1. 
 
-Approaches: Using Queue 
+Approach: Using Queue 
     The following are steps to print Bottom View of Binary Tree. 
     1. We put tree nodes in a queue for the level order traversal. 
     2. Start with the horizontal distance(hd) 0 of the root node, keep on adding left child to queue along with the horizontal distance as hd-1 and right child as hd+1. 
     3. Also, use a TreeMap which stores key value pair sorted on key. 
     4. Every time, we encounter a new horizontal distance or an existing horizontal distance put the node data for the horizontal distance as key. For the first time it will add to the map, next time it will replace the value. This will make sure that the bottom most element for that horizontal distance is present in the map and if you see the tree from beneath that you will see that element.
 
-	Time Complexity of the above implementation is O(n Log n). Note that the above implementation uses a map which is implemented using self-balancing BST. We can reduce the time complexity to O(n) using unordered_map. To print nodes in the desired order, we can have 2 variables denoting min and max horizontal distance. We can simply iterate from min to max horizontal distance and get corresponding values from Map. So it is O(n)
-    Auxiliary Space: O(n) 
+Time Complexity of the above implementation is O(n Log n). Note that the above implementation uses a map which is implemented using self-balancing BST. We can reduce the time complexity to O(n) using unordered_map. To print nodes in the desired order, we can have 2 variables denoting min and max horizontal distance. We can simply iterate from min to max horizontal distance and get corresponding values from Map. So it is O(n)
+Auxiliary Space: O(n) 
 
 */
 
