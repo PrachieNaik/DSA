579f77af0921fd4bfc41cc116cf7e09f8bc6a482
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-02-19 23:25:15 +0530

    Update CircularTour.cpp

--- Queue/CircularTour/CircularTour.cpp
+++ Queue/CircularTour/CircularTour.cpp
@@ -8,9 +8,16 @@ Note :  Assume for 1 litre petrol, the truck can go 1 unit of distance.
 
 Approaches:
 
-Method 1: A Simple Solution is to consider every petrol pumps as a starting point and see if there is a possible tour. If we find a starting point with a feasible solution, we return that starting point. The worst case time complexity of this solution is O(n^2).
+Method 1: A Simple Solution is to consider every petrol pumps as a starting point and see if there is a possible tour. If we find a starting point with a feasible 
+solution, we return that starting point. 
 
-Method 2: An efficient approach is to use a Queue to store the current tour. We first enqueue first petrol pump to the queue, we keep enqueueing petrol pumps till we either complete the tour, or the current amount of petrol becomes negative. If the amount becomes negative, then we keep dequeuing petrol pumps until the queue becomes empty.
+    Time Complexity: O(n^2)
+    Space Complexity: O(1)
+
+Method 2: An efficient approach is to use a Queue to store the current tour. We first enqueue first petrol pump to the queue, we keep enqueueing petrol pumps till
+we either complete the tour, or the current amount of petrol becomes negative. If the amount becomes negative, then we keep dequeuing petrol pumps until the queue 
+becomes empty.
+
     Time Complexity: O(N)
     Space Complexity: O(1)
 
