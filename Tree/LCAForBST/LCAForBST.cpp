3504d2bbd8bdc873206d30a421adf57940ad3661
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-02-19 22:13:19 +0530

    Update LCAForBST.cpp

--- Tree/LCAForBST/LCAForBST.cpp
+++ Tree/LCAForBST/LCAForBST.cpp
@@ -4,7 +4,10 @@ Problem statement: Given a Binary Search Tree (with all values unique) and two n
 
 Approaches:
 
-Method 1: For Binary search tree, while traversing the tree from top to bottom the first node which lies in between the two numbers n1 and n2 is the LCA of the nodes, i.e. the first node n with the lowest depth which lies in between n1 and n2 (n1<=n<=n2) n1 < n2. So just recursively traverse the BST in, if node's value is greater than both n1 and n2 then our LCA lies in the left side of the node, if it's is smaller than both n1 and n2, then LCA lies on the right side. Otherwise, the root is LCA (assuming that both n1 and n2 are present in BST).
+Method 1: For Binary search tree, while traversing the tree from top to bottom the first node which lies in between the two numbers n1 and n2 is the LCA of the 
+nodes, i.e. the first node n with the lowest depth which lies in between n1 and n2 (n1<=n<=n2) n1 < n2. So just recursively traverse the BST in, if node's value 
+is greater than both n1 and n2 then our LCA lies in the left side of the node, if it's is smaller than both n1 and n2, then LCA lies on the right side. 
+Otherwise, the root is LCA (assuming that both n1 and n2 are present in BST).
 
     Algorithm:
     
@@ -13,16 +16,17 @@ Method 1: For Binary search tree, while traversing the tree from top to bottom t
     If the value of the current node is greater than both n1 and n2, then LCA lies in the left subtree. Call the recursive function for thr left subtree.
     If both the above cases are false then return the current node as LCA.
     
-	Complexity Analysis:
+    Complexity Analysis:
 
     Time Complexity: O(h).
     The time Complexity of the above solution is O(h), where h is the height of the tree.
     Space Complexity: O(1).
     If recursive stack space is ignored, the space complexity of the above solution is constant.
 
-Method 2: Iterative Implementation: The above solution uses recursion. The recursive solution requires extra space in the form of the function call stack. So an iterative solution can be implemented which does not occupy space in the form of the function call stack.
+Method 2: Iterative Implementation: The above solution uses recursion. The recursive solution requires extra space in the form of the function call stack. 
+So an iterative solution can be implemented which does not occupy space in the form of the function call stack.
 	
-	Complexity Analysis:
+    Complexity Analysis:
 
     Time Complexity: O(h).
     The Time Complexity of the above solution is O(h), where h is the height of the tree.
