c2d50bdc280af3498d37232a43a2e298efd0a7d9
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-02-19 21:48:38 +0530

    Update DiameterOfTreeUsingDP.cpp

--- Tree/DiameterOfTree/DiameterOfTreeUsingDP.cpp
+++ Tree/DiameterOfTree/DiameterOfTreeUsingDP.cpp
@@ -1,7 +1,7 @@
 /*
 
 Problem statement: Given a Binary Tree, find diameter of it.
-The diameter of a tree is the number of nodes on the longest path between two end nodes in the tree. The diagram below shows two trees each with diameter nine, the leaves that form the ends of a longest path are shaded (note that there is more than one path in each tree of length nine, but no path longer than nine nodes).
+The diameter of a tree is the number of nodes on the longest path between two end nodes in the tree.
 
 Constraints:
 1 ≤ N ≤ 10^4
