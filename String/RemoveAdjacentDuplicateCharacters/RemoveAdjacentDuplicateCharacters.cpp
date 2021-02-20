c4fe2aeb2ffc826152650f2ce0756f8c7f5b66ca
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-02-20 10:29:42 +0530

    Update RemoveAdjacentDuplicateCharacters.cpp

--- String/RemoveAdjacentDuplicateCharacters/RemoveAdjacentDuplicateCharacters.cpp
+++ String/RemoveAdjacentDuplicateCharacters/RemoveAdjacentDuplicateCharacters.cpp
@@ -3,35 +3,39 @@
 Given a string, recursively remove adjacent duplicate characters from the string. The output string should not have any adjacent duplicates.
 
 Method 1: Keep removing duplicate characters till we reach a new character.
-Worst case time complexity: O(n^2). Space complexity: O(n). 
 
-string removedupli(string s) {
-    string result;
-    int resultIndex = 0;
-    int i = 0;
-    while(i < s.length()) {
-        if(s[i] == s[i+1]) {
-            int left = i;
-            int right = i+2;
-            while(right <s.length() && s[left] == s[right]) {
-                right++;
-            }
-            i = right;
-        }
-        else {
-            result.insert(resultIndex++, 1, s[i++]);
-        }
-    }
-    if(s.length() != result.length())
-        return removedupli(result);
-        
-    return result;
-}
+	Worst case time complexity: O(n^2). Space complexity: O(n). 
+
+	string removedupli(string s) {
+	    string result;
+	    int resultIndex = 0;
+	    int i = 0;
+	    while(i < s.length()) {
+		if(s[i] == s[i+1]) {
+		    int left = i;
+		    int right = i+2;
+		    while(right <s.length() && s[left] == s[right]) {
+			right++;
+		    }
+		    i = right;
+		}
+		else {
+		    result.insert(resultIndex++, 1, s[i++]);
+		}
+	    }
+	    if(s.length() != result.length())
+		return removedupli(result);
+
+	    return result;
+	}
 
 
-Method: Check for previous and next characters of current character, if none of them are same then we can add current character in the output string of that iteration. Do this till we don't find any consecutively same character in current iteration. 
-Worst case: Even length palindrome
-Worst case time complexity: O(n^2). Space complexity: O(n). 
+Method 2: Check for previous and next characters of current character, if none of them are same then we can add current character in the output string of that
+	iteration. Do this till we don't find any consecutively same character in current iteration. 
+	Worst case: Even length palindrome
+	
+	Worst case time complexity: O(n^2). 
+	Space complexity: O(n). 
 
 */
 
@@ -57,7 +61,6 @@ string removeDupli(string s)
 }
 int main()
  {
-	//code
 	int t;
 	cin>>t;
 	while(t--)
