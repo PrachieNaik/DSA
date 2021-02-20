55d9367b8abf6ab0a9c76b092c4137443a6f4775
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-02-20 10:10:37 +0530

    Update LongestDistinctCharactersSubString.cpp

--- String/LongestDistinctCharactersSubString/LongestDistinctCharactersSubString.cpp
+++ String/LongestDistinctCharactersSubString/LongestDistinctCharactersSubString.cpp
@@ -2,12 +2,15 @@
 
 Given a string S, find length of the longest substring with all distinct characters.
 
-
-Method 1 : The idea is to scan the string from left to right, keep track of the maximum length Non-Repeating Character Substring seen so far in res. When we traverse the string, to know the length of current window we need two indexes. 
+Method 1 : The idea is to scan the string from left to right, keep track of the maximum length Non-Repeating Character Substring seen so far in res. When we 
+traverse the string, to know the length of current window we need two indexes. 
 1) Ending index ( j ) : We consider current index as ending index. 
-2) Starting index ( i ) : It is same as previous window if current character was not present in the previous window. To check if the current character was present in the previous window or not, we store last index of every character in an array lasIndex[]. If lastIndex[str[j]] + 1 is more than previous start, then we updated the start index i. Else we keep same i. 
+2) Starting index ( i ) : It is same as previous window if current character was not present in the previous window. To check if the current character was present 
+in the previous window or not, we store last index of every character in an array lasIndex[]. If lastIndex[str[j]] + 1 is more than previous start, then we updated 
+the start index i. Else we keep same i. 
 
-Time Complexity: O(n + d) where n is length of the input string and d is number of characters in input string alphabet. For example, if string consists of lowercase English characters then value of d is 26. 
+Time Complexity: O(n + d) where n is length of the input string and d is number of characters in input string alphabet. For example, if string consists of 
+lowercase English characters then value of d is 26. 
 Auxiliary Space: O(d) 
 
 
@@ -22,8 +25,7 @@ int longestUniqueSubsttr(string str)
   
     int result = 0; 
   
-    // last index of all characters is initialized 
-    // as -1 
+    // last index of all characters is initialized as -1 
     vector<int> lastIndex(256, -1); 
   
     // Initialize start of current window 
@@ -32,10 +34,7 @@ int longestUniqueSubsttr(string str)
     // Move end of current window 
     for (int end = 0; end < n; end++) { 
   
-        // Find the last index of str[j] 
-        // Update i (starting index of current window) 
-        // as maximum of current value of i and last 
-        // index plus 1 
+        // Find the last index of str[j] Update i (starting index of current window) as maximum of current value of i and last index plus 1 
         start = max(start, lastIndex[str[end]] + 1); 
   
         // Update result if we get a larger window 
