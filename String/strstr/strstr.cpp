08ede9be0548ffd07dea66764620b1ab9295b793
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-02-20 10:50:19 +0530

    Update strstr.cpp

--- String/strstr/strstr.cpp
+++ String/strstr/strstr.cpp
@@ -1,13 +1,14 @@
 /*
 
-Implement the function strstr. The function takes two strings as arguments (s,x) and  locates the occurrence of the string x in the string s. The function returns and integer denoting the first occurrence of the string x in s (0 based indexing).
+Implement the function strstr. The function takes two strings as arguments (s,x) and  locates the occurrence of the string x in the string s.
+The function returns and integer denoting the first occurrence of the string x in s (0 based indexing).
 
 Edge cases: Target string length is greater then given string. 
 
-Method: The idea is to run a loop from start to end and for every index in the given string check whether the sub-string can be formed from that index. This can be done by running a nested loop traversing the given string and in that loop run another loop checking for sub-string from every index. 
+Method: The idea is to run a loop from start to end and for every index in the given string check whether the sub-string can be formed from that index. 
+This can be done by running a nested loop traversing the given string and in that loop run another loop checking for sub-string from every index. 
 
-Time complexity: O(m * n) where m and n are lengths of s1 and s2 respectively. A nested loop is used the outer loop runs from 0 to N-M and inner loop from 0 to M so the complexity is O(m*n).
-
+Time complexity: O(m * n) where m and n are lengths of s1 and s2 respectively. A nested loop is used the outer loop runs from 0 to N-M and inner loop from 0 to M 
 Space Complexity: O(1). 
 */
 
@@ -31,11 +32,8 @@ int main()
         cout<<strstr(a,b)<<endl;
     }
 }
-// } Driver Code Ends
 
-
-/* The function should return position where the target string 
-   matches the string str */
+/* The function should return position where the target string matches the string str */
 int strstr(string s, string x)
 {
      if(s.length() < x.length()) 
@@ -44,9 +42,7 @@ int strstr(string s, string x)
      for(int i=0; i<s.length()-x.length()+1 ;i++) {
          int indexOfS = i;
          int indexOfX = 0;
-         while ( indexOfS < s.length() 
-            && indexOfX < x.length() 
-            && s[indexOfS] == x[indexOfX]
+         while ( indexOfS < s.length() && indexOfX < x.length() && s[indexOfS] == x[indexOfX]
         ) {
              indexOfS ++;
              indexOfX ++;
