b4e828f5497de69a58ed4b1c00853df9f531e153
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-03-20 18:48:42 +0530

    Update LongestRepeatingSubsequence.cpp

--- DP/LCS/LongestRepeatingSubsequence.cpp
+++ DP/LCS/LongestRepeatingSubsequence.cpp
@@ -1,6 +1,7 @@
 /*
 
-Problem statement: Given a string, print the longest repeating subsequence such that the two subsequence don’t have same string character at same position, i.e., any i’th character in the two subsequences shouldn’t have the same index in the original string.
+Problem statement: Given a string, print the longest repeating subsequence such that the two subsequence don’t have same string character at same position, i.e., 
+any i’th character in the two subsequences shouldn’t have the same index in the original string.
 
 Input: str = "aabb"
 Output: "ab"
@@ -9,7 +10,8 @@ Input: str = "aab"
 Output: "a"
 The two subsequence are 'a'(first) and 'a' (second). Note that 'b' cannot be considered as part of subsequence as it would be at same index in both.
 
-Approch: This problem is just the modification of Longest Common Subsequence problem. The idea is to find the LCS(str, str) where str is the input string with the restriction that when both the characters are same, they shouldn’t be on the same index in the two strings.
+Approach: This problem is just the modification of Longest Common Subsequence problem. The idea is to find the LCS(str, str) where str is the input string with the 
+restriction that when both the characters are same, they shouldn’t be on the same index in the two strings.
 
 */
 
