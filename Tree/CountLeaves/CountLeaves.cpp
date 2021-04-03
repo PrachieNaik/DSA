4df0232a7cc2caf5df6cce01615ac10bbe070d7a
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-04-03 14:59:56 +0530

    Update CountLeaves.cpp

--- Tree/CountLeaves/CountLeaves.cpp
+++ Tree/CountLeaves/CountLeaves.cpp
@@ -3,14 +3,13 @@
 Problem statement: Given a Binary Tree of size N , You have to count leaves in it.
 
 Constraints:
-1<= N <= 104
+1<= N <= 10^4
 
 Approach: getLeafCount(node)
     1) If node is NULL then return 0.
     2) Else If left and right child nodes are NULL return 1.
     3) Else recursively calculate leaf count of the tree using below formula.
-        Leaf count of a tree = Leaf count of left subtree + 
-                                     Leaf count of right subtree
+        Leaf count of a tree = Leaf count of left subtree + Leaf count of right subtree
      
     Time complexity – O(n)  
 
