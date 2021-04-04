69e3280686cc32971c290669117755bd23e60d21
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-04-04 16:33:12 +0530

    Update KadaneAlgorithm.cpp

--- Array/KadaneAlgorithm/KadaneAlgorithm.cpp
+++ Array/KadaneAlgorithm/KadaneAlgorithm.cpp
@@ -2,7 +2,9 @@
 
 Write an efficient program to find the sum of contiguous subarray within a one-dimensional array of numbers which has the largest sum.
 
-Kadane's Algorithm: Simple idea of the Kadane's algorithm is to look for all positive contiguous segments of the array (max_ending_here is used for this). And keep track of maximum sum contiguous segment among all positive segments (max_so_far is used for this). Each time we get a positive sum compare it with max_so_far and update max_so_far if it is greater than max_so_far
+Kadane's Algorithm: Simple idea of the Kadane's algorithm is to look for all positive contiguous segments of the array (max_ending_here is used for this). And 
+keep track of maximum sum contiguous segment among all positive segments (max_so_far is used for this). Each time we get a positive sum compare it with max_so_far 
+and update max_so_far if it is greater than max_so_far
 
 Time Complexity: O(n), Space Complexity: O(1)
 */
