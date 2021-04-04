901846ddfc622e57b9e1425d64db4aab42312a46
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-04-04 21:30:33 +0530

    Update 01KnapSackUsingDP.cpp

--- DP/01KnapSack/01KnapSackUsingDP.cpp
+++ DP/01KnapSack/01KnapSackUsingDP.cpp
@@ -1,6 +1,9 @@
 /*
 
-Problem statement: You are given weights and values of N items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. Note that we have only one quantity of each item. In other words, given two integer arrays val[0..N-1] and wt[0..N-1] which represent values and weights associated with N items respectively. Also given an integer W which represents knapsack capacity, find out the maximum value subset of val[] such that sum of the weights of this subset is smaller than or equal to W. You cannot break an item, either pick the complete item, or don’t pick it (0-1 property).
+Problem statement: You are given weights and values of N items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. Note 
+that we have only one quantity of each item. In other words, given two integer arrays val[0..N-1] and wt[0..N-1] which represent values and weights associated with
+N items respectively. Also given an integer W which represents knapsack capacity, find out the maximum value subset of val[] such that sum of the weights of this 
+subset is smaller than or equal to W. You cannot break an item, either pick the complete item, or don’t pick it (0-1 property).
 
 Input:
 N = 3
@@ -21,11 +24,14 @@ Keywords: Maximum, Choice, Optimize
 Approach:
 
     In a DP[][] table let’s consider all the possible weights from ‘1’ to ‘W’ as the columns and weights that can be kept as the rows. 
-    The state DP[i][j] will denote maximum value of ‘j-weight’ considering all values from ‘1 to ith’. So if we consider ‘wi’ (weight in ‘ith’ row) we can fill it in all columns which have ‘weight values > wi’. Now two possibilities can take place: 
+    The state DP[i][j] will denote maximum value of ‘j-weight’ considering all values from ‘1 to ith’. So if we consider ‘wi’ (weight in ‘ith’ row) we can fill it 
+    in all columns which have ‘weight values > wi’. Now two possibilities can take place: 
     
     Fill ‘wi’ in the given column.
     Do not fill ‘wi’ in the given column.
-    Now we have to take a maximum of these two possibilities, formally if we do not fill ‘ith’ weight in ‘jth’ column then DP[i][j] state will be same as DP[i-1][j] but if we fill the weight, DP[i][j] will be equal to the value of ‘wi’+ value of the column weighing ‘j-wi’ in the previous row. So we take the maximum of these two possibilities to fill the current state.
+    Now we have to take a maximum of these two possibilities, formally if we do not fill ‘ith’ weight in ‘jth’ column then DP[i][j] state will be same as
+    DP[i-1][j] but if we fill the weight, DP[i][j] will be equal to the value of ‘wi’+ value of the column weighing ‘j-wi’ in the previous row. So we take the 
+    maximum of these two possibilities to fill the current state.
     
     Complexity Analysis: 
 
