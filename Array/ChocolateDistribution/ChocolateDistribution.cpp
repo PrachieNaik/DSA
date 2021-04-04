7a12565843680bdec66631b568c70c55a2dd02e5
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-04-04 14:19:13 +0530

    Update ChocolateDistribution.cpp

--- Array/ChocolateDistribution/ChocolateDistribution.cpp
+++ Array/ChocolateDistribution/ChocolateDistribution.cpp
@@ -1,17 +1,17 @@
 /*
 
-Given an array of n integers where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are m students, the task is to distribute chocolate packets such that: 
+Given an array of n integers where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are m 
+students, the task is to distribute chocolate packets such that: 
 
 1. Each student gets one packet.
 2. The difference between the number of chocolates in the packet with maximum chocolates and packet with minimum chocolates given to the students is minimum.
 
-Method: Based on the observation that to minimize the difference, we must choose consecutive elements from a sorted packet. We first sort the array arr[0..n-1], then find the subarray of size m with the minimum difference between the last and first elements.
+Method: Based on the observation that to minimize the difference, we must choose consecutive elements from a sorted packet. We first sort the array arr[0..n-1], 
+then find the subarray of size m with the minimum difference between the last and first elements.
 
 Time Complexity: O(n Log n), Space Complexity: O(1)
 
 */
-
-
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -54,12 +54,9 @@ int findMinDiff(int arr[], int n, int m)
 
 int main()
 {
-	int arr[] = {12, 4, 7, 9, 2, 23, 25, 41,
-				30, 40, 28, 42, 30, 44, 48,
-				43, 50};
+	int arr[] = {12, 4, 7, 9, 2, 23, 25, 41, 30, 40, 28, 42, 30, 44, 48, 43, 50};
 	int m = 7; // Number of students
 	int n = sizeof(arr)/sizeof(arr[0]);
-	cout << "Minimum difference is "
-		<< findMinDiff(arr, n, m);
+	cout << "Minimum difference is " << findMinDiff(arr, n, m);
 	return 0;
 }
