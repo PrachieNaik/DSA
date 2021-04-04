0f69352a8530070613e1c28a1dd3e0b3d2e038ea
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-04-04 14:29:14 +0530

    Update ElementWithLeftSmallerRightGreater.cpp

--- Array/ElementWithLeftSmallerRightGreater/ElementWithLeftSmallerRightGreater.cpp
+++ Array/ElementWithLeftSmallerRightGreater/ElementWithLeftSmallerRightGreater.cpp
@@ -6,7 +6,9 @@ Method: Create two arrays leftMax[] and rightMin[].
 Traverse input array from left to right and fill leftMax[] such that leftMax[i] contains a maximum element from 0 to i-1 in the input array.
 Traverse input array from right to left and fill rightMin[] such that rightMin[i] contains a minimum element from to n-1 to i+1 in the input array.
 Traverse input array. For every element arr[i], check if arr[i] is greater than leftMax[i] and smaller than rightMin[i]. If yes, return i.
-Further Optimization to the above approach is to use only one extra array and traverse input array only twice. The first traversal is the same as above and fills leftMax[]. Next traversal traverses from the right and keeps track of the minimum. The second traversal also finds the required element. This method will return first valid element from right.
+Further Optimization to the above approach is to use only one extra array and traverse input array only twice. The first traversal is the same as above and fills
+leftMax[]. Next traversal traverses from the right and keeps track of the minimum. The second traversal also finds the required element. This method will return 
+first valid element from right.
 
 Time Complexity:  O(n) , Auxiliary Space: O(1).
 
