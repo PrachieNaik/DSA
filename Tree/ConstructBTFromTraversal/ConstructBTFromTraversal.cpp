b7b1347935660b53712637064a5d06ba3d773048
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-05-02 15:22:43 +0530

    Create ConstructBTFromTraversal.cpp

---
+++ Tree/ConstructBTFromTraversal/ConstructBTFromTraversal.cpp
@@ -0,0 +1,18 @@
+/*
+
+Problem statement: If you are given two traversal sequences, can you construct the binary tree?
+
+It depends on what traversals are given. If one of the traversal methods is Inorder then the tree can be constructed, otherwise not.
+
+Therefore, following combination can uniquely identify a tree.
+
+Inorder and Preorder.
+Inorder and Postorder.
+Inorder and Level-order.
+
+And following do not.
+Postorder and Preorder.
+Preorder and Level-order.
+Postorder and Level-order.
+
+*/
