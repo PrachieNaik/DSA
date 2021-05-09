43a4f49b82aaa6414d1f43ff29d1f7c259925fbc
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-05-10 01:35:28 +0530

    Update UnboundedKnapSack.cpp

--- DP/UnboundedKnapSack/UnboundedKnapSack.cpp
+++ DP/UnboundedKnapSack/UnboundedKnapSack.cpp
@@ -1,6 +1,7 @@
 /*
 
-Problem statement: Given a set of N items, each with a weight and a value, and a weight limit W. Find the maximum value of a collection containing any of the N items any number of times so that the total weight is less than or equal to W.
+Problem statement: Given a set of N items, each with a weight and a value, and a weight limit W. Find the maximum value of a collection containing any of 
+the N items any number of times so that the total weight is less than or equal to W.
 
 Input: N = 2, W = 3
 val[] = {1, 1}
@@ -14,7 +15,8 @@ Constraints:
 
 Approches:
 
-Method 1: We can include as many instances of items as we want. If we decide to not include an element, then we can take it as processed and move ahead. So with a little modification to traditional knap sack, this problem can be solved with below equation.
+Method 1: We can include as many instances of items as we want. If we decide to not include an element, then we can take it as processed and move ahead. 
+So with a little modification to traditional knap sack, this problem can be solved with below equation.
 
     if(wt[i - 1] <= j) {
         dp[i][j] = val[i-1] + dp[i][j - wt[i - 1]];
@@ -24,7 +26,9 @@ Method 1: We can include as many instances of items as we want. If we decide to 
         dp[i][j] = dp[i - 1][j];
     }
 
-Method 2: A simple 1D array, say dp[W+1] can be used such that dp[i] stores the maximum value which can achieved using all items and i capacity of knapsack. Note that we use 1D array here which is different from classical knapsack where we used 2D array. Here number of items never changes. We always have all items available.
+Method 2: A simple 1D array, say dp[W+1] can be used such that dp[i] stores the maximum value which can achieved using all items and i capacity of knapsack.
+Note that we use 1D array here which is different from classical knapsack where we used 2D array. Here number of items never changes. We always have all 
+items available.
 
     We can recursively compute dp[] using below formula
     
