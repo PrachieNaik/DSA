0cd2181bc8beea26fc721a25da1c5bd9b15ca486
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-05-10 00:47:48 +0530

    Update MCMusingMemoization.cpp

--- DP/MCM/MCMusingMemoization.cpp
+++ DP/MCM/MCMusingMemoization.cpp
@@ -1,12 +1,15 @@
 /*
 
-Problem statement: Given a sequence of matrices, find the most efficient way to multiply these matrices together. The efficient way is the one that involves the least number of multiplications. The dimensions of the matrices are given in an array arr[] of size N (such that N = number of matrices + 1) where the ith matrix has the dimensions (arr[i-1] x arr[i]).
+Problem statement: Given a sequence of matrices, find the most efficient way to multiply these matrices together. The efficient way is the one that involves
+the least number of multiplications. The dimensions of the matrices are given in an array arr[] of size N (such that N = number of matrices + 1) where the
+ith matrix has the dimensions (arr[i-1] x arr[i]).
 
 Input: N = 5
 arr = {40, 20, 30, 10, 30}
 
 Output: 26000
-Explaination: There are 4 matrices of dimension 40x20, 20x30, 30x10, 10x30. Say the matrices are named as A, B, C, D. The efficient way is (A*(B*C))*D. The number of operations are 20*30*10 + 40*20*10 + 40*10*30 = 26000.
+Explaination: There are 4 matrices of dimension 40x20, 20x30, 30x10, 10x30. Say the matrices are named as A, B, C, D. The efficient way is (A*(B*C))*D. The
+number of operations are 20*30*10 + 40*20*10 + 40*10*30 = 26000.
 
 Approch: Memoization
 
