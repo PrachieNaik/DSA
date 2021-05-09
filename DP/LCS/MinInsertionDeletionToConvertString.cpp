42dadf0e6604a44e74db491e4f5d35ff261b8f5b
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-05-09 23:37:10 +0530

    Update MinInsertionDeletionToConvertString.cpp

--- DP/LCS/MinInsertionDeletionToConvertString.cpp
+++ DP/LCS/MinInsertionDeletionToConvertString.cpp
@@ -1,6 +1,8 @@
 /*
 
-Problem statement: Given two strings ‘str1’ and ‘str2’ of size m and n respectively. The task is to remove/delete and insert the minimum number of characters from/in str1 to transform it into str2. It could be possible that the same character needs to be removed/deleted from one point of str1 and inserted to some another point.
+Problem statement: Given two strings ‘str1’ and ‘str2’ of size m and n respectively. The task is to remove/delete and insert the minimum number of characters 
+from/in str1 to transform it into str2. It could be possible that the same character needs to be removed/deleted from one point of str1 and inserted to some
+another point.
 
 Input : 
 str1 = "heap", str2 = "pea" 
@@ -10,7 +12,8 @@ Minimum Deletion = 2 and
 Minimum Insertion = 1
 
 Explanation:
-p and h deleted from heap. Then, p is inserted at the beginning. One thing to note, though p was required yet it was removed/deleted first from its position and then it is inserted to some other position. Thus, p contributes one to the deletion_count and one to the insertion_count.
+p and h deleted from heap. Then, p is inserted at the beginning. One thing to note, though p was required yet it was removed/deleted first from its position and
+then it is inserted to some other position. Thus, p contributes one to the deletion_count and one to the insertion_count.
 
 Constraints:
 1 ≤ |str1|, |str2| ≤ 1000
