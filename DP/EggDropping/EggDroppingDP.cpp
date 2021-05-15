801cfe18d507eab7a34d7b6c80828bd57e629f2e
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-05-15 14:19:10 +0530

    Create EggDroppingDP.cpp

---
+++ DP/EggDropping/EggDroppingDP.cpp
@@ -0,0 +1,59 @@
+/*
+Problem statement: Suppose you have N eggs and you want to determine from which floor in a K-floor building you can drop an egg such that it doesn't break. 
+You have to determine the minimum number of attempts you need in order find the critical floor in the worst case while using the best strategy.There are few
+rules given below. 
+    An egg that survives a fall can be used again.
+    A broken egg must be discarded.
+    The effect of a fall is the same for all eggs.
+    If the egg doesn't break at a certain floor, it will not break at any floor below.
+    If the eggs breaks at a certain floor, it will break at any floor above.
+    
+Constraints:
+1<=N<=10
+1<=K<=50
+
+Approach: Dynamic Programming
+*/
+
+/* Function to get minimum
+number of trials needed in worst
+case with n eggs and k floors */
+int eggDrop(int n, int k)
+{
+    /* A 2D table where entry
+    eggFloor[i][j] will represent
+    minimum number of trials needed for
+    i eggs and j floors. */
+    int eggFloor[n + 1][k + 1];
+    int res;
+    int i, j, x;
+ 
+    // We need one trial for one floor and 0
+    // trials for 0 floors
+    for (i = 1; i <= n; i++) {
+        eggFloor[i][1] = 1;
+        eggFloor[i][0] = 0;
+    }
+ 
+    // We always need j trials for one egg
+    // and j floors.
+    for (j = 1; j <= k; j++)
+        eggFloor[1][j] = j;
+ 
+    // Fill rest of the entries in table using
+    // optimal substructure property
+    for (i = 2; i <= n; i++) {
+        for (j = 2; j <= k; j++) {
+            eggFloor[i][j] = INT_MAX;
+            for (x = 1; x <= j; x++) {
+                res = 1 + max( eggFloor[i - 1][x - 1], eggFloor[i][j - x]);
+                if (res < eggFloor[i][j])
+                    eggFloor[i][j] = res;
+            }
+        }
+    }
+ 
+    // eggFloor[n][k] holds the result
+    return eggFloor[n][k];
+}
+ 
