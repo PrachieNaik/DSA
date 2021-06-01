87121c93e74d9146b84dcfd103ffa371a5f8ae62
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2021-06-01 14:42:49 +0530

    Update BFS.cpp

--- Graph/BFS/BFS.cpp
+++ Graph/BFS/BFS.cpp
@@ -1,6 +1,8 @@
 /*
 
-Problem statement: Given a directed graph. The task is to do Breadth First Traversal of this graph starting from 0. Note: One can move from node u to node v only if there's an edge from u to v and find the BFS traversal of the graph starting from the 0th vertex, from left to right according to the graph. Also, you should only take nodes directly or indirectly connected from Node 0 in consideration.
+Problem statement: Given a directed graph. The task is to do Breadth First Traversal of this graph starting from 0. Note: One can move from node u to 
+node v only if there's an edge from u to v and find the BFS traversal of the graph starting from the 0th vertex, from left to right according to the graph.
+Also, you should only take nodes directly or indirectly connected from Node 0 in consideration.
 
 Constraints: 1 ≤ V, E ≤ 10^4
 
